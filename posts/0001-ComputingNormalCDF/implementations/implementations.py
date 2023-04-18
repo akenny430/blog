c3f30af3d49573cd92c7879ced1a9e89166ead84
Aiden Kenny <git.akenny430@gmail.com>
2023-04-18 04:13:29 -0400

    added speed testing

--- posts/0001-ComputingNormalCDF/implementations/implementations.py
+++ posts/0001-ComputingNormalCDF/implementations/implementations.py
@@ -1,44 +1,38 @@
-x_vals = [-3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0]
+import time 
+from typing import Callable 
+
+import numpy as np 
+
+# for testing 
+from scipy.stats import norm as Norm 
+from statistics import NormalDist 
+
+x_vals: list[float] = [-3.0 + (n * 0.01) for n in range(0, 602)]
+print(f"Testing normal distribution on {len(x_vals)} values stored in a list of floats ... ")
+
+def time_imp(imp: Callable[..., None]) -> Callable[..., None]:
+    def temp_func() -> None: 
+        t0: float = time.perf_counter_ns() 
+        imp() 
+        t1: float = time.perf_counter_ns() 
+        print(f"Time of {imp.__name__}: {t1 - t0:,} ns")
+        return 
+    return temp_func
 
 
-def imp01_UsingScipy() -> None: 
-    from scipy.stats import norm # has cdf 
-
-    cdf_vals = norm.cdf(x_vals)
-
-    print("\nUsing norm from scipy.stats: ")
-    for (x_val, cdf_val) in zip(x_vals, cdf_vals): 
-        print(f"{x_val:>5}: {cdf_val:0.7f}")
-    """
-    -3.0: 0.0013499
-    -2.0: 0.0227501
-    -1.0: 0.1586553
-    0.0: 0.5000000
-    1.0: 0.8413447
-    2.0: 0.9772499
-    3.0: 0.9986501
-    """
+@time_imp
+def imp01_scipy() -> None: 
+    _: np.ndarray = Norm.cdf(x_vals)  
+    return 
 
 
-def imp02_UsingStatistics() -> None: 
-    from statistics import NormalDist 
-
-    cdf_vals = [NormalDist().cdf(x_val) for x_val in x_vals] 
-
-    print("\nUsing NormalDist from statistics: ")
-    for (x_val, cdf_val) in zip(x_vals, cdf_vals): 
-        print(f"{x_val:>5}: {cdf_val:0.7f}")
-    """
-    -3.0: 0.0013499
-    -2.0: 0.0227501
-    -1.0: 0.1586553
-    0.0: 0.5000000
-    1.0: 0.8413447
-    2.0: 0.9772499
-    3.0: 0.9986501
-    """
+@time_imp
+def imp02_statistics() -> None: 
+    norm: NormalDist = NormalDist(0.0, 1.0)
+    _: list[float] = [norm.cdf(x_val) for x_val in x_vals] 
+    return 
 
 
 if __name__ == "__main__": 
-    imp01_UsingScipy()
-    imp02_UsingStatistics()
+    imp01_scipy()
+    imp02_statistics()
