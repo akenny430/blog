c201cbd7431180cbaf5228673e6391d310281976
Aiden Kenny <git.akenny430@gmail.com>
2023-04-18 03:24:06 -0400

    initial README

---
+++ README.md
@@ -0,0 +1,16 @@
+# Aiden's Blog 
+
+I like to write about topics in mathematics, computer science, and quantitative finance. 
+
+There is no set goal for this blog other than to help me jot down ideas or improve my understanding, 
+and perhaps others may find my thoughts useful or interesting. 
+
+The types of posts one can expect are: 
+- A more detailed derivation/implementation of a well-known topic. 
+- An exploratory analysis of an idea. 
+- An explanation of a topic that I am more familiar with. 
+- A comment/reaction to current events. 
+
+## List of posts 
+
+- [0001: Fast and correct computation of the standard normal CDF](posts/0001-ComputingNormalCDF/README.md)
---
+++ update_readme.py
@@ -0,0 +1,37 @@
+import os 
+from pathlib import Path 
+
+_BLOG_PATH: Path = Path.cwd() 
+_POSTS_PATH: Path = _BLOG_PATH / "posts" 
+
+_INTRO: str = (
+"""# Aiden's Blog 
+
+I like to write about topics in mathematics, computer science, and quantitative finance. 
+
+There is no set goal for this blog other than to help me jot down ideas or improve my understanding, 
+and perhaps others may find my thoughts useful or interesting. 
+
+The types of posts one can expect are: 
+- A more detailed derivation/implementation of a well-known topic. 
+- An exploratory analysis of an idea. 
+- An explanation of a topic that I am more familiar with. 
+- A comment/reaction to current events. 
+
+## List of posts 
+
+"""
+)
+
+
+
+with open(file=_BLOG_PATH / "README.md", mode='w') as readme:
+    # print(type(readme)) # TextIOWrapper   
+    readme.write(_INTRO) 
+    for blog_post in os.listdir(_POSTS_PATH): 
+        print( str(_POSTS_PATH / blog_post / "README.md") )
+        with open(file=_POSTS_PATH / blog_post / "README.md", mode='r') as blog_md: 
+            blog_code, blog_title = blog_md.readline().split(':') 
+            blog_code = blog_code.replace("# ", '') 
+            blog_title = blog_title.replace('\n', '').strip()
+        readme.write(f"- [{blog_code}: {blog_title}](posts/{blog_post}/README.md) ") # TODO: not sure about Path concatonation
