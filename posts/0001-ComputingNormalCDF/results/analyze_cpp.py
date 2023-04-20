79022a67e96a3137fac43549e3f0137c4c3ea79b
Aiden Kenny <git.akenny430@gmail.com>
2023-04-20 04:27:17 -0400

    making plot for taylor series

---
+++ posts/0001-ComputingNormalCDF/results/analyze_cpp.py
@@ -0,0 +1,26 @@
+import polars as pl 
+import plotnine as pn 
+
+_COOL_PHI: str = "$\Phi(x)$"
+cpp_df: pl.DataFrame = (
+    pl
+    .scan_csv(source="./cpp_results.csv")
+    .melt(
+        id_vars="x", 
+        variable_name="Implementation", 
+        value_name=_COOL_PHI, 
+    )
+    .collect() 
+)
+print(cpp_df) 
+
+plot_results: pn.ggplot = (
+    pn.ggplot(
+        data=cpp_df.to_pandas(), 
+        mapping=pn.aes(x="x", y=_COOL_PHI)
+    )
+    + pn.geom_line(color="blue") 
+    + pn.facet_wrap(facets="Implementation")
+    + pn.theme_bw() 
+)
+plot_results.save(filename="./cpp_plot.svg", verbose=False) 
